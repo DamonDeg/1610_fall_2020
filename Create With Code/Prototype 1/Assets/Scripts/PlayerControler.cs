cc60f7d49965bfc3832d460c27e854813c4c5275
Damon Degler <10867687@my.uvu.edu>
2020-09-22 22:38:46 -0600

    Create with code stuff

---
+++ Create With Code/Prototype 1/Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    public float speed = 10;
+    public float turnSpeed = 5f;
+    public float horizontalInput = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        //move the vehicle forward
+        transform.Translate(0,0,speed * Time.deltaTime);
+        transform.Translate(Time.deltaTime * turnSpeed * horizontalInput,0,0);
+    }
+}
