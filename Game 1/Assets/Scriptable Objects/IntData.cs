c49e2ffa207e5093ea096cdb68d6c3295d64e185
Damon Degler <10867687@my.uvu.edu>
2020-11-06 22:58:34 -0700

    My Game Stuff
    wanted to get more but was struggling and my internet turns off at 11

--- Game 1/Assets/Scriptable Objects/IntData.cs
+++ Game 1/Assets/Scriptable Objects/IntData.cs
@@ -2,17 +2,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [CreateAssetMenu]
 public class IntData : ScriptableObject
 {
     public int value;
-
+    public Image bar;
     public void UpdateValue(int number)
     {
 
         value+=number;
         Debug.Log(value);
 
+    }
+    
+    public void updateBar()
+    {
+        bar.fillAmount = value;
     }
 }
---
+++ Game 1/Assets/Scripts/ActionResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ActionResponse : MonoBehaviour
+{
+    public GameAction actionObj;
+    public UnityEvent handlerEvent;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        actionObj.action += Handle;
+    }
+
+    private void Handle()
+    {
+        handlerEvent.Invoke();
+    }
+}
