254ab9261264df54475ee4a2a6f778c9906d0230
Damon Degler <10867687@my.uvu.edu>
2020-11-12 00:00:42 -0700

    h

---
+++ Create With Code/New Unity Project/Assets/Scripts.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Scripts : ScriptableObject
+{
+
+
+    public enum weapons
+    {
+        sword,
+        axe,
+        sheild,
+        locket,
+        spoon,
+        acid,
+        grassClump,
+        h
+    }
+
+    public void useWeapon()
+    {
+        switch (weapons)
+        {
+            case weapons.acid:
+                break;
+            case weapons.acid:
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+
+}
