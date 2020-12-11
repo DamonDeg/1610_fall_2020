a7d65567deb65ef03795651a49fd7778664a6294
Damon Degler <10867687@my.uvu.edu>
2020-12-10 22:54:22 -0700

    game stuff

---
+++ Create With Code/time warp test/Assets/Scripts/GlobalVars.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalVars : MonoBehaviour
+{
+    public float globalTimeMult, globalGravity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
