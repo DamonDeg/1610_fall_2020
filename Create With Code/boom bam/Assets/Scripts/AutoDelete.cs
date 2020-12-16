0c4825c31863aac191dff948a9e1b260bdd4e8f8
Damon Degler <10867687@my.uvu.edu>
2020-12-15 23:19:15 -0700

    wee
    for loop yay

---
+++ Create With Code/boom bam/Assets/Scripts/AutoDelete.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDelete : MonoBehaviour
+{
+    private int destroyTimer = 1;
+    // Start is called before the first frame update
+    void Update()
+    {
+        if (destroyTimer <= 0)
+        {
+            Destroy(gameObject);
+        }
+        destroyTimer--;
+    }
+    
+}
---
+++ Create With Code/boom bam/Assets/Scripts/SpawnDaCubes.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnDaCubes : MonoBehaviour
+{
+    public GameObject daCube;
+    public int clusterSize;
+    public float clusterSpread;
+    private Vector3 placePos = new Vector3();
+
+    // Update is called once per frame
+    void Update()
+    {
+        //makes a cluster of cubes based on the var clusterSize
+        for(int i=0; i<clusterSize; i++)
+        {
+            //chooses a random spot
+            placePos.x = transform.position.x + (Random.Range(-clusterSpread, clusterSpread));
+            placePos.y = transform.position.y + (Random.Range(-clusterSpread, clusterSpread));
+            placePos.z = transform.position.z + (Random.Range(-clusterSpread, clusterSpread));
+            
+            //places the cube
+            Instantiate(daCube, placePos, daCube.transform.rotation);
+        }
+    }
+}
